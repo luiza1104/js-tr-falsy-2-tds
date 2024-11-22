eeebbcdc767d2a76a8aa90b670cfc2a4ba472dd0
Luíza Fernandes <160490311+luiza1104@users.noreply.github.com>
2024-11-22 11:57:53 -0300

    carga_js-tr-falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,31 @@
+const x = 0;
+//Boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou false
+// 0 -> false
+// 1 -> true 
+
+console.log(0== false)//returna true
+console.log(""== false)//retorna true
+console.log(1== true)
+
+console.log("null e undefinned");
+//undefined
+// null-> vazio ou nada
+
+let minhaVar;
+let varNUll=null;
+
+console.log("Quando o undefined está em sua forma pura");
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
